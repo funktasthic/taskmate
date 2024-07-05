aa47bbad12d0fd20f7323a485d813792ead26345
Funktasthic <ignacio.avendano@alumnos.ucn.cl>
2024-07-05 12:24:38 -0400

    feat: add theme selector

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react';
 import Logo from '../assets/logo.svg';
 
 export const Header = () => {
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme ? JSON.parse(savedTheme) : 'dark';
+  });
+
+  useEffect(() => {
+    localStorage.setItem('theme', JSON.stringify(theme));
+    document.documentElement.removeAttribute('class');
+    document.documentElement.classList.add(theme);
+  }, [theme]);
+
   return (
     <header>
       <div className='logo'>
@@ -8,8 +20,14 @@ export const Header = () => {
         <span>Taskmate</span>
       </div>
       <div className='themeSelector'>
-        <span className='light'></span>
-        <span className='dark activeTheme'></span>
+        <span
+          onClick={() => setTheme('light')}
+          className={`${theme === 'light' ? 'activeTheme' : ''} light`}
+        ></span>
+        <span
+          onClick={() => setTheme('dark')}
+          className={`${theme === 'dark' ? 'activeTheme' : ''} dark`}
+        ></span>
       </div>
     </header>
   );
